1120f77d375f7f740eb9cf1e11436cf641c542b6
uk45h <l.leszewski@gmail.com>
2017-12-09 16:19:49 +0100

    trycatch fix

--- motionSensor.py
+++ motionSensor.py
@@ -19,7 +19,13 @@ try:
 except KeyboardInterrupt:  
     # here you put any code you want to run before the program   
     # exits when you press CTRL+C  
-    print "\n", counter # print value of counter  
+    print "\n przerwane na żądanie" # print value of counter  
+
+except:  
+    # this catches ALL other exceptions including errors.  
+    # You won't get any error messages for debugging  
+    # so only use it once your code is working  
+    print "Other error or exception occurred!"  
 
 finally:  
     GPIO.cleanup() # this ensures a clean exit  

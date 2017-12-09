8a025fca8f4f7ae2330b908185cb8c3f58d9b734
uk45h <l.leszewski@gmail.com>
2017-12-09 16:18:03 +0100

    trycatch

--- motionSensor.py
+++ motionSensor.py
@@ -1,15 +1,25 @@
 import RPi.GPIO as GPIO                           #Import GPIO library
 import time                                       #Import time library
 GPIO.setmode(GPIO.BOARD)                          #Set GPIO pin numbering
-PIR = 26                                          #Associate pin 26 to pir
+PIR = 21                                         #Associate pin 26 to pir
 GPIO.setup(PIR, GPIO.IN)                          #Set pin as GPIO in 
 print "Waiting for sensor to settle"
 time.sleep(2)                                     #Waiting 2 seconds for the sensor to initiate
 print "Detecting motion"
-while True:
-   if GPIO.input(PIR):                            #Check whether pir is HIGH
-      print "Motion Detected!"
-      time.sleep(2)                               #D1- Delay to avoid multiple detection
-   else:
-   	  print "No motion!"
-   time.sleep(0.1)                                #While loop delay should be less than detection(hardware) delay
+
+try:
+	while True:
+	   if GPIO.input(PIR):                            #Check whether pir is HIGH
+	      print "Motion Detected!"
+	      time.sleep(2)                               #D1- Delay to avoid multiple detection
+	   else:
+	   	  print "No motion!"
+	   time.sleep(0.5)                                #While loop delay should be less than detection(hardware) delay
+
+except KeyboardInterrupt:  
+    # here you put any code you want to run before the program   
+    # exits when you press CTRL+C  
+    print "\n", counter # print value of counter  
+
+finally:  
+    GPIO.cleanup() # this ensures a clean exit  

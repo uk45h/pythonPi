32a7e701f073862668fc67c9cc1acdc1cd0e9d19
uk45h <l.leszewski@gmail.com>
2020-01-18 18:30:45 +0100

    print values

--- pmSensorSleep.py
+++ pmSensorSleep.py
@@ -2,8 +2,10 @@ from sds011 import SDS011
 import time
 
 sensor = SDS011("/dev/ttyUSB0", use_query_mode=True)
-sensor.query()
+pmValue = sensor.query()
+print(pmValue)
 sensor.sleep()
 sensor.sleep(sleep=False) 
 time.sleep(15)
-sensor.query()
+pmValue = sensor.query()
+print(pmValue)
